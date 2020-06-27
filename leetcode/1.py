75afd6ef0c86ed97fcfc1324a6d0a9651040bd2e
zjhj <zj_hj2010@163.com>
2020-06-27 22:40:37 +0800

    leetcode start

---
+++ leetcode/1.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+class Solution:
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums, target):
+        return True
+
+a = Solution()
+a.twoSum( [2, 7, 11, 15],9 )
